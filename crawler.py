efd57d048b0b72c8dfe1fbc3f8bf10d108026031
Rok Zidarn <rz3817@student.uni-lj.si>
2019-03-17 12:18:20 +0100

    Added DB inserts

--- crawler.py
+++ crawler.py
@@ -12,6 +12,9 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine, MetaData, Column, ForeignKey
 from sqlalchemy.orm import sessionmaker, relationship, query
 from sqlalchemy.dialects.mysql import TEXT, VARCHAR, INTEGER, TIMESTAMP, LONGBLOB, CHAR
+import datetime
+import sys
+
 
 # http://edmundmartin.com/multi-threaded-crawler-in-python/
 class Crawler:
@@ -19,30 +22,39 @@ class Crawler:
     def __init__(self, seed_urls, num_workers):
         self.pool = ThreadPoolExecutor(max_workers=num_workers)  # parallel crawling, multiple workers
         self.scraped_pages = set([''])  # set of already scraped pages, needed to test duplication
+        self.scraped_sites = set([''])  # set of already scraped sites
         self.frontier = Queue()  # BFS implementation, FIFO
         for seed in seed_urls:
             self.frontier.put(seed)
 
-    def extract_links_images(self, base_url, driver):
-        html = driver.page_source
-        links = driver.find_elements_by_xpath("//a[@href]")
-        images = driver.find_elements_by_xpath("//img[@src]")
-        root_url = '{}://{}'.format(urlparse(base_url).scheme, urlparse(base_url).netloc)  # canonical
+    def create_session(self):
+        meta = MetaData(schema="crawldb")
+        Base = declarative_base(metadata=meta)
+        DATABASE_URI = 'postgres+psycopg2://postgres:rokzidarn@localhost:5432/crawldb'
 
-        m = hashlib.md5()
-        encoded = bytes(html, 'utf-8')
-        m.update(encoded)
-        hashed = m.digest()  # hash function on HTML code, check for duplication
-        # TODO: compare exact HTML code (md5 hash function) -> DB access
+        engine = create_engine(DATABASE_URI)
+        Base.metadata.create_all(engine)
+        Session = sessionmaker(bind=engine)
 
-        try:  # quick fix for Python SSL error
-            status_code = requests.get(base_url).status_code
-        except:
-            driver.close()
-            return
+        return Session()
 
-        robots = Robots.fetch(root_url + '/robots.txt')  # robots.txt file
-        print('ROBOTS: ', robots)
+    def delete_all(self):
+        session = self.create_session()
+
+        session.query(Image).delete()
+        session.commit()
+        session.query(Link).delete()
+        session.commit()
+        session.query(PageData).delete()
+        session.commit()
+        session.query(Page).delete()
+        session.commit()
+        session.query(Site).delete()
+        session.commit()
+
+        session.close()
+
+    def insert_site(self, root_url, robots):
         sitemaps = list(robots.sitemaps)  # get sitemaps
 
         if len(sitemaps) > 0:
@@ -54,34 +66,146 @@ class Crawler:
                 if candidate not in self.scraped_pages:
                     self.frontier.put(candidate)
 
-        for link in links:  # extract links
-            url = link.get_attribute('href')
-            if 'javascript' not in url and 'mailto' not in url and not url.startswith('#') \
-                    and urlparse(url).netloc[-7:] == '.gov.si':  # URL, domain conditions
+        session = self.create_session()
 
-                if url.startswith('/') or url.startswith(root_url):  # solve relative links
-                    # if not (re.search("#|\?", url))
-                    url = urljoin(root_url, url)
+        if root_url not in self.scraped_sites:
+            print('ROBOTS: ', robots)
+            site = Site(
+                domain=root_url,
+                robots_content=str(robots),
+                sitemap_content=sitemaps
+            )
+            session.add(site)
+            session.commit()
 
-                if url[-3:] == 'pdf' or url[-3:] == 'doc' or url[-3:] == 'ppt' \
-                        or url[-4:] == 'pptx' or url[-4:] == 'docx':  # extract files
-                    file_base64 = base64.b64encode(requests.get(url).content)
-                    print('FILE: ', file_base64)
-                    continue
+            self.scraped_sites.add(root_url)
+            site_id = site.id
+        else:
+            site = session.query(Site).filter(Site.domain == root_url).first()
+            site_id = site.id
 
-                if url not in self.scraped_pages and robots.allowed(url, '*'):
-                    self.frontier.put(url)  # if page is not duplicated and is allowed in robots.txt, add to frontier
+        session.close()
+        return site_id
 
-        # TODO: onclick Javascript events (location.href or document.location)
+    def insert_page(self, site_id, base_url, html):
+        try:  # quick fix for Python SSL error
+            status_code = requests.get(base_url).status_code
+        except:
+            driver.close()
+            return None
 
-        for image in images:
-            src = image.get_attribute('src')
-            if src.startswith('http'):  # only add images with URL source, discard others
-                if src.startswith('/'):
-                    src = urljoin(root_url, src)
+        md5 = hashlib.md5()  # compare exact HTML code (md5 hash function)
+        encoded = bytes(html, 'utf-8')
+        md5.update(encoded)
+        hashed = md5.digest()  # hash function on HTML code, check for duplication
 
-                image_base64 = base64.b64encode(requests.get(src).content)
-                print('IMAGE: ', image_base64)
+        session = self.create_session()
+
+        pages = session.query(Page).all()
+        hashes = [page.hash for page in pages]
+        if hashed not in hashes:
+            page = Page(
+                site_id=site_id,
+                page_type_code='HTML',
+                url=base_url,  # UNIQUE
+                html_content=html,
+                http_status_code=status_code,
+                accessed_time=datetime.datetime.now().date(),
+                hash=hashed
+            )
+            session.add(page)
+            session.commit()
+            page_id = page.id
+        else:
+            page_id = None
+
+        session.close()
+        return page_id
+
+    def insert_image(self, page_id, src, encoded):
+        session = self.create_session()
+        content_type = src.split('.')
+        filename = content_type[-2].split('/')
+        print('IMAGE: ', src)
+
+        image = Image(
+            page_id=page_id,
+            filename=filename[-1],
+            content_type=content_type[-1],
+            data=encoded,
+            accessed_time=datetime.datetime.now().date()
+        )
+
+        session.add(image)
+        session.commit()
+        session.close()
+
+    def insert_page_data(self, site_id, url, encoded):
+        session = self.create_session()
+        content_type = url.split('.')[-1].upper()
+        print('FILE: ', url)
+
+        page = Page(
+            site_id=site_id,
+            page_type_code='BINARY',
+            url=url,  # UNIQUE
+            http_status_code=200,
+            accessed_time=datetime.datetime.now().date(),
+        )
+        session.add(page)
+        session.commit()
+        page_id = page.id
+
+        page_data = PageData(
+            page_id=page_id,
+            data_type_code=content_type,
+            data=encoded
+        )
+
+        session.add(page_data)
+        session.commit()
+        session.close()
+
+    def extract_links_images(self, base_url, driver):
+        html = driver.page_source
+        links = driver.find_elements_by_xpath("//a[@href]")
+        images = driver.find_elements_by_xpath("//img[@src]")
+        root_url = '{}://{}'.format(urlparse(base_url).scheme, urlparse(base_url).netloc)  # canonical
+        robots = Robots.fetch(root_url + '/robots.txt')  # robots.txt file
+
+        site_id = self.insert_site(root_url, robots)
+        page_id = self.insert_page(site_id, base_url, html)
+
+        if page_id is not None:
+            for link in links:  # extract links
+                url = link.get_attribute('href')
+                if 'javascript' not in url and 'mailto' not in url and not url.startswith('#') \
+                        and urlparse(url).netloc[-7:] == '.gov.si':  # URL, domain conditions
+
+                    if url.startswith('/') or url.startswith(root_url):  # solve relative links
+                        # if not (re.search("#|\?", url))
+                        url = urljoin(root_url, url)
+
+                    if url[-3:] == 'pdf' or url[-3:] == 'doc' or url[-3:] == 'ppt' \
+                            or url[-4:] == 'pptx' or url[-4:] == 'docx':  # extract files
+
+                        file_base64 = base64.b64encode(requests.get(url).content)
+                        self.insert_page_data(site_id, url, file_base64)
+                        continue
+
+                    if url not in self.scraped_pages and robots.allowed(url, '*') and ('#' not in url):
+                        self.frontier.put(url)  # if page is not duplicated and is allowed in robots, add to frontier
+
+            # TODO: onclick Javascript events (location.href or document.location)
+
+            for image in images:
+                src = image.get_attribute('src')
+                if src.startswith('http'):  # only add images with URL source, discard others
+                    if src.startswith('/'):
+                        src = urljoin(root_url, src)
+
+                    image_base64 = base64.b64encode(requests.get(src).content)
+                    self.insert_image(page_id, src, image_base64)
 
         driver.close()
 
@@ -90,7 +214,7 @@ class Crawler:
         options.add_argument("headless")
         options.add_experimental_option("prefs", {"profile.default_content_settings.cookies": 2})  # disable cookies
         driver = webdriver.Chrome(options=options)
-        driver.implicitly_wait(10)  # TODO: ERROR: fix stale element reference: element is not attached to page document
+        driver.implicitly_wait(5)  # TODO: ERROR: fix stale element reference: element is not attached to page document
 
         try:
             driver.get(url)
@@ -115,6 +239,7 @@ class Crawler:
                     job = self.pool.submit(self.scrape_page, url)  # setup driver, get page from URL
                     job.add_done_callback(self.post_scrape_callback)  # get/save data to DB
             except Empty:  # if queue is empty for 60s stop crawling
+                session.close()
                 return
             except Exception as e:  # ignore all other exceptions
                 print(e)
@@ -124,8 +249,14 @@ class Crawler:
 # MAIN
 if __name__ == '__main__':
     # ['http://evem.gov.si', 'https://e-uprava.gov.si', 'https://podatki.gov.si', 'http://www.e-prostor.gov.si']
-    seed_urls = ['http://evem.gov.si']
-    s = Crawler(seed_urls, 5)  # number of workers
-    s.run_crawler()
+    seed_urls = ['https://e-uprava.gov.si', 'https://podatki.gov.si', 'http://www.e-prostor.gov.si']
+    # sys.stdout = open('data/stdout.txt', 'w')
+    crawl = Crawler(seed_urls, 5)  # number of workers
+    crawl.delete_all()
+    crawl.run_crawler()
     # TODO: detect redirects
     # TODO: crawl delay
+    # TODO: pgAdmin4 dump DB
+    # TODO: image duplicates?
+    # TODO: .zip, .cls URLs?
+    # TODO: Links table (+PK), FRONTIER and DUPLICATE PageType
